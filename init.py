f49bd682798eae69e352e608d7067a01840e8a02
anishfish2 <anishkarthik21@gmail.com>
2023-07-31 10:32:34 -0500

    Tidying up files to ensure consistency when initializing index's

--- init.py
+++ init.py
@@ -3,13 +3,25 @@ import sys
 import time
 import yaml
 
+#Function to ensure index has been fully initalized
+def wait_on_index(serverName):
+  
+  ready = False
 
+  while not ready:
+    try:
+      desc = pinecone.describe_index(serverName)
+      if desc[7]['ready']:
+        return True
+      
+    except pinecone.core.client.exceptions.NotFoundException:
+      pass
 
 if __name__ == '__main__':
     
+    print('Running init.py')
+    
     serverName = sys.argv[1]
-
-    print('Creating index', serverName)
 
     def read_yaml(file_path):
         with open(file_path, "r") as f:
@@ -25,10 +37,8 @@ if __name__ == '__main__':
     # Create pinecone index and load
     pinecone.init(api_key=api_key, environment=environment)
 
-    print('Before creation:', pinecone.list_indexes())
-
     pinecone.create_index(serverName, dimension=8, metric="euclidean")
 
-    time.sleep(60)
-
-    print('After creation:', pinecone.list_indexes())
+    print("During init.py:", pinecone.list_indexes())
+    wait_on_index(serverName)
+    time.sleep(30)
--- quickstart.py
+++ quickstart.py
@@ -2,17 +2,29 @@ import pinecone
 import yaml
 import os
 import sys
-import subprocess
+import time
 
 # Modified from Pinecone Quickstart -> https://docs.pinecone.io/docs/quickstart
-
 # Load API data from config.yaml
+
+#Function to ensure index has been fully initalized
+def wait_on_index(serverName):
+  ready = False
+
+  while not ready:
+    try:
+      desc = pinecone.describe_index(serverName)
+      if desc[7]['ready']:
+        return True
+      
+    except pinecone.core.client.exceptions.NotFoundException:
+      pass
 
 if __name__ == '__main__':
 
     serverName = sys.argv[1]
 
-    print('Server name:', serverName)
+    print(serverName)
 
     def read_yaml(file_path):
         with open(file_path, "r") as f:
@@ -31,11 +43,21 @@ if __name__ == '__main__':
     if len(pinecone.list_indexes()) == 0:
         os.system("python init.py " + serverName)
 
-
-    print('Current indexes:', pinecone.list_indexes())
+    if serverName not in pinecone.list_indexes():
+        for i in pinecone.list_indexes():
+            pinecone.delete_index(i)
+        os.system("python init.py " + serverName)
     
     index = pinecone.Index(serverName)
 
+    print(pinecone.list_indexes())
+
+    #Without this line, if index did not exist, http error will throw because of incomplete index intitialization
+    wait_on_index(serverName)
+
+    print(pinecone.list_indexes())
+
+    #Print stats before upsert
     print(index.describe_index_stats())
 
     # Insert Dummy data
@@ -47,6 +69,7 @@ if __name__ == '__main__':
         ("E", [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
     ])
 
+    #Print stats after upsert
     print(index.describe_index_stats())
     # # Returns:
     # # {'dimension': 8, 'index_fullness': 0.0, 'namespaces': {'': {'vector_count': 5}}}
@@ -69,11 +92,5 @@ if __name__ == '__main__':
     #               'values': [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]}],
     #  'namespace': ''}
 
-    # Delete index
-
-        
-    print("Before deletion:", pinecone.list_indexes())
-
-    pinecone.delete_index(serverName)
-
-    print("After deletion:", pinecone.list_indexes())
+    # Delete index -> Uncomment if you would like index to be deleted
+    #pinecone.delete_index(serverName)
